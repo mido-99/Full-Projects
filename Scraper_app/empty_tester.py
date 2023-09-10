ca9c32321638466966919562043ac5f9ec1ddf97
mido <midohany910@gmail.com>
2023-09-10 16:13:17 +0300

    Some code enhancements, some explanatoin

--- Scraper_app/empty_tester.py
+++ Scraper_app/empty_tester.py
@@ -30,7 +30,7 @@ class MainApp(QMainWindow):
     def handle_buttons(self):
         self.pushButton_2.clicked.connect(self.add_field)
         self.pushButton.clicked.connect(self.get_save_path)
-        self.pushButton_3.clicked.connect(self.start_crawling)
+        self.pushButton_3.clicked.connect(self.start_crawling)  #*2
         
         
     def add_field(self):
@@ -42,12 +42,15 @@ class MainApp(QMainWindow):
         myQlistItem.setSizeHint(QSize(400, 50))
         self.listWidget.setItemWidget(myQlistItem, myCustom)
     
+    # These setters and getters are used to retrieve data in spider file from current 
+    # running app instance
     def set_data(self, data):
         self.data_list = [data]
-        
+            
     def get_data(self):
-        return self.data_list
-        
+        return self.url, self.data_list
+
+    
     def get_user_input(self):
         """Retrieve data from QlineEdit widgets in a custom Listitem Widget"""
                 
@@ -71,7 +74,7 @@ class MainApp(QMainWindow):
     def get_url(self):
         """Fetch start url"""
 
-        url = self.lineEdit.text()
+        self.url = self.lineEdit.text()
     
     def get_save_path(self):
         """Get file save location and type"""
@@ -86,7 +89,7 @@ class MainApp(QMainWindow):
         self.get_save_path()
         self.get_user_input()
 
-        process = CrawlerProcess(get_project_settings())
+        process = CrawlerProcess(get_project_settings()) #*4
         process.crawl('scraper_app')
         process.start()
         
@@ -97,7 +100,7 @@ import globals
 if __name__ == '__main__':
     app = QApplication([])
     widget = MainApp()
-    globals.main_app_instance = widget
+    globals.main_app_instance = widget  #*3
     widget.show()
     app.exec()
 
@@ -106,4 +109,40 @@ if __name__ == '__main__':
 #* This line works like the opposite side of the coin for setItemWidget(QlistItem, custom).
 the latter is used to change a normal item into a custom widget, while the former is used 
 to transform an item to the custom one, so we can access its methods and attributs.
+
+
+#*2 Breakdown of what is going on when the button is clicked:
+First of all DATA collection; this happens in get_url(), get_user_input(), and 
+get_save_path() To assign the values of self.url, self.data_list and save respectively.
+
+Our goal is to pass these data to our spider, but we can't jsut make a global variables
+with their names cause they will be imported empty at the beginning of the script, and 
+any reassinging to their values to update it will cause the imported vars not to reflect
+these changes.
+(Maybe it can work with lists since they're immutable and imports just refer to them in
+memory, but still will cause problems when reassigning and won't work with strings at all)
+
+So the idea was to def getters and setters to set their values, and retrieve them later
+from the curent running instance of the MainApp in spider file.
+But How to refer to the currently running instance? we don't instantiate it manually, 
+it's instantiated when the app runs. Here comes point 3.
+
+
+#*3 Reference to the current App instance
+To be able to do it, I created a new file (globals) with just a dull empty var.
+its value is assigned in main file exactly after creating an instance of MainApp, Then 
+in the spider, I import the var from (globals). Since this instance reflects the curent
+running MainApp; I can use its getter to retrieve the data in spider. And That's All
+
+
+#*4 Running Spider from another script in same project
+As mentioned before, in this project I have some imoprts from main file in spider file,
+so I can't make the opposite -to refer to the Spider class- so to avoid circular imports.
+
+Here comes the use of CrawlProcess(get_project_settings()), To run the spider.
+process.crawl() usually takes a class as argument, and since I can't import the class;
+I'm using the other way of writing its name as a string.
+Last line of process.start() To start the crawling process.
+
+
 """
--- Scraper_app/scraper_app/spiders/scraper_app.py
+++ Scraper_app/scraper_app/spiders/scraper_app.py
@@ -3,6 +3,8 @@ import scrapy
 from globals import main_app_instance
 
 
+url, data_list = main_app_instance.get_data()
+
 class Scraper(scrapy.Spider):
     name = 'scraper_app'
     # start_urls = [f'{url}']
@@ -10,13 +12,9 @@ class Scraper(scrapy.Spider):
 
     def parse(self, response):
         
-        data_list = main_app_instance.get_data()
         for h in response.css('h2.css-m604qf'):
             print(h.css('a::text').get())
         
-        print(data_list)
-    
-    # def scrape_data(self):
-    #     for row in data_list:
-    #         print (row)
-
+    def scrape_data(self):
+        for dict in data_list:
+            pass
